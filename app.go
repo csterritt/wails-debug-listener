0044b464ed43e68bf47cfba42f5dd9959f1abbce
Chris Sterritt <csterritt@gmail.com>
2024-02-12 19:59:23 -0500

    Fiber listener set up, sends JSON of messages up to front end.

--- app.go
+++ app.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
@@ -13,24 +13,48 @@ type App struct {
 	ctx context.Context
 }
 
+type ShowLine struct {
+	Line   string `json:"line"`
+	Sender string `json:"sender"`
+	Type   string `json:"type"`
+}
+
+func setupListener(wailsApp *App) {
+	listenerApp := fiber.New()
+
+	listenerApp.Post("/", func(c *fiber.Ctx) error {
+		line := new(ShowLine)
+		if err := c.BodyParser(line); err != nil {
+			return err
+		}
+
+		runtime.LogPrintf(wailsApp.ctx, "%s: %s %s\n", line.Sender, line.Type, line.Line)
+
+		runtime.EventsEmit(wailsApp.ctx, "incoming", line)
+
+		return nil
+	})
+
+	port := 3030
+	err := listenerApp.Listen(fmt.Sprintf(`:%d`, port))
+	if err != nil {
+		panic(err)
+	}
+}
+
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	app := &App{}
+
+	return app
 }
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-}
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	go func() {
-		time.Sleep(2 * time.Second)
-		a.SendUp("Greet sending up for " + name)
-	}()
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+	setupListener(a)
 }
 
 func (a *App) SendUp(info string) string {
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,17 +1,7 @@
 <template>
   <main>
     <div>Hello, world!</div>
-
-    <button class="btn btn-primary" @click="dingBell">Ring the bell</button>
   </main>
 </template>
 
-<script lang="ts" setup>
-import { Greet } from '../../wailsjs/go/main/App'
-
-const dingBell = async () => {
-  console.log(`dingBell called`)
-  const ans = await Greet('foobar')
-  console.log(`... Greet returns ${JSON.stringify(ans)}`)
-}
-</script>
+<script lang="ts" setup></script>
